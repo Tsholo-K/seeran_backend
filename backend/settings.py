95581bc566474b7b71443b62380290f6b1d2aab4
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-28 17:28:04 +0200

    fix 2

--- backend/settings.py
+++ backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'seerandatabase-2',
         'USER': 'seeran',
         'PASSWORD': 'kegpim-tagjo1-Zoqbak',
-        'HOST': '172.31.48.57',
+        'HOST': 'seerandatabase-2.cz4cqeskmn2k.af-south-1.rds.amazonaws.com',
         'PORT': '5432',  # Default PostgreSQL port
     }
 }
