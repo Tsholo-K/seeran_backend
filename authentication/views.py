9a30d6a1fbc89d69d07eabd0995183a8946b5cb5
Addyh0pe <tsholo.koketso@icloud.com>
2024-02-29 16:29:15 +0200

    login test

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+import json
+
 
 # Create your views here.
 def login(request):
-    ...
+    if request.method == 'POST':
+        user_data = json.loads(request.body)
+        if user_data.get('email') == 'lethabo.mochaki1076@icloud.com' & user_data.get('email') == 'lethabo.mochaki1076@icloud.com':
+            return JsonResponse('authenticated')
+        else:
+            return JsonResponse('invalid user')
