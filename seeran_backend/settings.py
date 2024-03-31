ccd2e2df84d197e6f66a110211d307445719b005
Addyh0pe <tsholo.koketso@icloud.com>
2024-03-31 21:39:46 +0200

    rework

--- seeran_backend/settings.py
+++ seeran_backend/settings.py
@@ -139,8 +139,12 @@ WSGI_APPLICATION = 'seeran_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'seeran_database',
+        'USER': 'tsholo',
+        'PASSWORD': 'N4Hoj5Mjrw4BEGvfWZAB',
+        'HOST': 'seeran-database.cz4cqeskmn2k.af-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
