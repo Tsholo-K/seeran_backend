1ce6338f04a10dad5197d0e7520d8f32c7d867e3
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-01 02:32:01 +0200

    ssh

--- seeran_backend/settings.py
+++ seeran_backend/settings.py
@@ -150,6 +150,13 @@ DATABASES = {
     }
 }
 
+# SSH
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
