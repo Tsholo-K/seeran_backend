080abef92244585d466c8aa602b2945177ee5627
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-01 13:48:58 +0200

    settings

--- seeran_backend/settings.py
+++ seeran_backend/settings.py
@@ -7,26 +7,26 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1hrd@6u+u$0ouahd*z)v5ra+hu1nn&ljum=oh(r0i3noxbsg7i'
 
+
+# aws config
+# access keys 
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+
+# activates debug mode for the application
 DEBUG = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
 
-if not DEBUG:
-    ALLOWED_HOSTS = ["*"]
-    
-ALLOWED_HOSTS = ["*"]
+# SECURITY WARNING: don't run with debug turned on in production!
+# sets the ip addresses the application can be hosted from
+ALLOWED_HOSTS = ["*"] 
 
 
 # Application definition
-# installed apps
-
+# a list of all installed apps
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,14 +36,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # project apps 
-    'authentication',
-    'schools',
+    'authentication', # users/security/authentication
+    'schools', # schools/locations/logos
     
     # third party apps
+    'storages' # allows for application to read/write to s3 bucket
 ]
 
-# project middleware
 
+# project middleware
+# all project middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     
@@ -51,7 +53,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     
     # projects middleware
-    'authentication.middleware.TokenValidationMiddleware',
+    'authentication.middleware.TokenValidationMiddleware', # middleware for token authentication and renewal
     
     # django middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -62,8 +64,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# cors config
 
+# cors config
+# origins/domains allowed to communicate with the application
 if not DEBUG:
     CORS_ALLOWED_ORIGINS = [
         'https://www.seeran-grades.com',
@@ -76,18 +79,37 @@ CORS_ALLOWED_ORIGINS = [
     # Add other allowed origins as needed
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+
+# cors credentials
+# allows credentials (cookies, authorization headers, or TLS client certificates) to be sent in cross-origin requests.
+CORS_ALLOW_CREDENTIALS = True 
+
 
 # rest framework config
-
+# default authentication method
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
-# simplejwt config
 
+# applications default user model
+# our custom user model 
+AUTH_USER_MODEL = 'authentication.CustomUser'
+
+
+# user authenticator
+# our custom authentication backend
+AUTHENTICATION_BACKENDS = [
+    'authentication.auth_backends.EmailOrIdNumberModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    # Add other authentication backends if needed
+]
+
+
+# simplejwt config
+# simplejwt token settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),  # Short-lived access token (adjust as needed)
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),    # Longer-lived refresh token (adjust as needed)
@@ -98,8 +120,9 @@ SIMPLE_JWT = {
     # Other settings (e.g., ALGORITHM, SIGNING_KEY, etc.) can be customized as well
 }
 
-# Caching config
 
+# Caching config
+# applications caching configuration
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
@@ -110,11 +133,48 @@ CACHES = {
     }
 }
 
+
+# Database
+# connection credentials for postgres rds instance
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'seeran_database',
+        'USER': 'tsholo',
+        'PASSWORD': 'N4Hoj5Mjrw4BEGvfWZAB',
+        'HOST': 'seeran-database.cz4cqeskmn2k.af-south-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+
+# s3 bucket
+# s3 bucket configuration
+AWS_STORAGE_BUCKET_NAME = 'seeran-storage'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 # Email sending config
 
 
-# default settings 
+# ssl config
+# configures the application to commmunicate in https
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
+
+# static files (CSS, JavaScript, Images)
+# static files location
+STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+
+
+# default settings 
+# the rest are default django settigns
 ROOT_URLCONF = 'seeran_backend.urls'
 
 TEMPLATES = [
@@ -133,34 +193,13 @@ TEMPLATES = [
     },
 ]
 
+
 # seeran_backend/settings.py
 WSGI_APPLICATION = 'seeran_backend.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'seeran_database',
-        'USER': 'tsholo',
-        'PASSWORD': 'N4Hoj5Mjrw4BEGvfWZAB',
-        'HOST': 'seeran-database.cz4cqeskmn2k.af-south-1.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
-
-# SSH
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -177,36 +216,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# user authenticator
-
-AUTHENTICATION_BACKENDS = [
-    'authentication.auth_backends.EmailOrIdNumberModelBackend',
-    'django.contrib.auth.backends.ModelBackend',
-    # Add other authentication backends if needed
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# default user model
-AUTH_USER_MODEL = 'authentication.CustomUser'
