9eb85a4c60ccc274531fa9f0f1e5d6a13836b38f
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-03 19:43:06 +0200

    rework

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.login, name='token obtain pair'),
     path('signin/', views.signin, name='first time sign in'),
     path('verifyotp/', views.verify_otp_view, name='otp authentication'),
+    path('setpassword/', views.set_password_view, name='set account password'),
     path('accountstatus/', views.account_activated, name='check if account is activated'),
     path('resendotp/', views.resend_otp, name='request new otp'),
     path('logout/', views.user_logout, name='user logout'),
