4668e4e6e065715599ebc9175b65f8dda58e22b7
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-12 17:20:27 +0200

    update

--- seeran_backend/settings.py
+++ seeran_backend/settings.py
@@ -185,6 +185,7 @@ with open(os.path.join(BASE_DIR, 'private_key.pem')) as aws_cert:
 AWS_STORAGE_BUCKET_NAME = 'seeran-storage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'af-south-1'
 
 
 # Email sending config
