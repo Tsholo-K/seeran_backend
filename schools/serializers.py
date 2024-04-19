1573c322ac3e5766fc8d497b376ff680d66385f7
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-19 21:47:57 +0200

    update

--- schools/serializers.py
+++ schools/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
+
+# models
 from .models import School
+from authentication.models import CustomUser
+
 
 class SchoolSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = School
         fields = ['name', 'email', 'contact_number', 'school_type', 'province', 'school_district', 'school_id']
+
+class SchoolsSerializer(serializers.ModelSerializer):
+    
+    learners = serializers.SerializerMethodField()
+    parents = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = School
+        fields = [
+            'name',
+            'learners',
+            'parents',
+            'number_of_classrooms',
+        ]
+        
+    def get_learners(self, obj):
+        return CustomUser.objects.filter(school=obj, role='STUDENT').count()
+
+    def get_parents(self, obj):
+        return CustomUser.objects.filter(school=obj, role='PARENT').count()
--- schools/views.py
+++ schools/views.py
@@ -3,9 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # models
+from .models import School
 
 # serializers
-from .serializers import SchoolSerializer
+from .serializers import SchoolSerializer, SchoolsSerializer
 
 # custom decorators
 from authentication.decorators import token_required
@@ -18,6 +19,15 @@ from .decorators import founder_only
 def create_school(request):
     serializer = SchoolSerializer(data=request.data)
     if serializer.is_valid():
-        #serializer.save()
+        serializer.save()
         return Response({"message" : serializer.data}, status=201)
     return Response({"error" : serializer.errors})
+
+
+@api_view(['GET'])
+@token_required
+@founder_only
+def schools(request):
+    schools = School.objects.all()
+    serializer = SchoolsSerializer(schools, many=True)
+    return Response(serializer.data)
