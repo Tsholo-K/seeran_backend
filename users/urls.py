e2bce3f412d961fcbfa72bf383cf8e16434c3aa0
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-21 16:41:20 +0200

    mb

--- users/urls.py
+++ users/urls.py
@@ -10,10 +10,7 @@ urlpatterns = [
     path('my-id/<int:invalidator>/', views.my_id, name='returns user info'),
     path('my-profile/<int:invalidator>/', views.my_profile, name='returns user info'),
     path('my-details/<int:invalidator>/', views.my_details, name='returns user info'),
-    path('my-profile-picture/<int:invalidator>/', views.my_image, name='returns user profile image'),
     path('my-security-info/<int:invalidator>/', views.my_security_info, name='returns user profile image'),
-    path('my-email/<int:invalidator>/', views.my_email, name='returns user email'),
-    path('my-names/<int:invalidator>/', views.my_names, name='returns user name and surname'),
     
     # profile picture change 
     path("update-profile-picture/", views.update_profile_picture, name="update user profile picture"),
