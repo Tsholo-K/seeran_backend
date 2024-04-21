3ca2fd08f36d48c5dea55588f8ce3d1ebc08970f
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-21 15:14:45 +0200

    mb

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # my info
     path('my-id/<int:invalidator>/', views.my_id, name='returns user info'),
     path('my-profile/<int:invalidator>/', views.my_profile, name='returns user info'),
-    path('my-details/<int:invalidator>/', views.my_profile, name='returns user info'),
+    path('my-details/<int:invalidator>/', views.my_details, name='returns user info'),
     path('my-profile-picture/<int:invalidator>/', views.my_image, name='returns user profile image'),
     path('my-email/<int:invalidator>/', views.my_email, name='returns user email'),
     path('my-names/<int:invalidator>/', views.my_names, name='returns user name and surname'),
