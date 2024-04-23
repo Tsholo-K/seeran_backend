0ec3e259332602ec9ea4c4a49b12800a792f2aac
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-23 12:00:42 +0200

    update

--- schools/urls.py
+++ schools/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('create-school/', views.create_school, name="create school account"),
     path('schools/<int:invalidator>/', views.schools, name="get all school accounts"),
     path('school/<str:school_id>/<int:invalidator>/', views.school, name="get school info"),
-    path('school-info/<str:school_id>/<int:invalidator>/', views.school_info, name="get school info"),
 ]
