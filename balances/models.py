b0aabb331fe67dd0728906a9493af00b64df87d1
Addyh0pe <tsholo.koketso@icloud.com>
2024-04-23 19:01:31 +0200

    update

--- balances/models.py
+++ balances/models.py
@@ -1,5 +1,6 @@
 # python 
 import datetime
+from dateutil.relativedelta import relativedelta
 
 # django
 from django.db import models
@@ -8,21 +9,22 @@ from django.utils import timezone
 # models 
 from users.models import CustomUser
 
-
 # utility 
 from authentication.utils import generate_account_id
+
+
 
 class Balance(models.Model):
     
     user = models.OneToOneField( CustomUser, on_delete=models.CASCADE, limit_choices_to={'role__in': ['PRINCIPAL', 'STUDENT']}, related_name='balance' )
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     last_updated = models.DateTimeField(auto_now=True)
-    billing_date = models.DateField()
+    billing_date = models.DateField(default=(timezone.now() + relativedelta(months=1)).replace(day=7))
+    
+    balance_id = models.CharField(max_length=15, unique=True, default=generate_account_id('BL'))
 
     def __str__(self):
         return f'{self.user.email if self.user.email else self.user.id_number} owes {self.amount}'
-
-
 
 
 class Bill(models.Model):
