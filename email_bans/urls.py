bdb9360fd762807b82add7f931111494f753229e
Addyh0pe <tsholo.koketso@icloud.com>
2024-05-05 14:08:42 +0200

    mb

--- email_bans/urls.py
+++ email_bans/urls.py
@@ -3,12 +3,9 @@ from . import views
 
 urlpatterns = [
     
-    # email ban/appeals urls,
+    # email ban urls,
     path('email-bans/', views.email_bans, name="return users email bans"),
     path('email-ban/<str:email_ban_id>/<int:invalidator>/', views.email_ban, name="return a specific email ban"),
-    
-    path('email-ban-appeals/', views.email_ban_appeals, name="return all unresolved email bana appeals"),
-    path('email-ban-appeal/<str:email_ban_id>/', views.email_ban_appeal, name="return a specific email ban appeal"),
     
     path('appeal/<str:email_ban_id>/', views.appeal, name="user email ban appeal end point"),
     
