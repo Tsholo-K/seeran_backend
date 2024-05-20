877716434d0fa9ed41a50a844257db85accfb3d8
Addyh0pe <tsholo.koketso@icloud.com>
2024-05-20 16:05:02 +0200

    update

--- seeran_backend/settings.py
+++ seeran_backend/settings.py
@@ -163,7 +163,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
-        'USER': config('USER'),
+        'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': '5432',
