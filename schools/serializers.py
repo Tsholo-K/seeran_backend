a6f6599ac6d2ad7fed7488029608e8038ded85e7
Tsholo Koketso <101592023+Tsholo-K@users.noreply.github.com>
2024-05-23 14:12:57 +0200

    update

--- schools/serializers.py
+++ schools/serializers.py
@@ -67,45 +67,13 @@ class SchoolsSerializer(serializers.ModelSerializer):
 
 class SchoolSerializer(serializers.ModelSerializer):
         
-    name = serializers.SerializerMethodField()
     principal = serializers.SerializerMethodField()
     balance = serializers.SerializerMethodField()
-    students = serializers.SerializerMethodField()
-    parents = serializers.SerializerMethodField()
-    teachers = serializers.SerializerMethodField()
-    admins = serializers.SerializerMethodField()
-    school_type = serializers.SerializerMethodField()
-    school_district = serializers.SerializerMethodField()
-    province = serializers.SerializerMethodField()
 
     class Meta:
         model = School
-        fields = ['name', 'school_type', 'school_district',  'province', 'email', 'contact_number', 'school_id', 'principal', 'balance',  'students', 'parents', 'teachers', 'admins', ]
-        
-    def get_name(self, obj):
-        return obj.name.title()
-    
-    def get_school_type(self, obj):
-        return obj.school_type.title()
-
-    def get_school_district(self, obj):
-        return obj.school_district.title()
-
-    def get_province(self, obj):
-        return obj.province.title()
-
-    def get_students(self, obj):
-        return CustomUser.objects.filter(role='STUDENT', school=obj).count()
-
-    def get_parents(self, obj):
-        return CustomUser.objects.filter(role='PARENT', school=obj).count()
-
-    def get_teachers(self, obj):
-        return CustomUser.objects.filter(role='TEACHER', school=obj).count()
-
-    def get_admins(self, obj):
-        return CustomUser.objects.filter(Q(role='ADMIN') | Q(role='PRINCIPAL'), school=obj).count()
-        
+        fields = ['principal', 'balance' ]
+                
     def get_principal(self, obj):
     
         try:
@@ -179,3 +147,42 @@ class SchoolSerializer(serializers.ModelSerializer):
             return None
     
     
+class SchoolDetailsSerializer(serializers.ModelSerializer):
+        
+    name = serializers.SerializerMethodField()
+    students = serializers.SerializerMethodField()
+    parents = serializers.SerializerMethodField()
+    teachers = serializers.SerializerMethodField()
+    admins = serializers.SerializerMethodField()
+    school_type = serializers.SerializerMethodField()
+    school_district = serializers.SerializerMethodField()
+    province = serializers.SerializerMethodField()
+
+    class Meta:
+        model = School
+        fields = ['name', 'school_type', 'school_district',  'province', 'email', 'contact_number', 'school_id',  'students', 'parents', 'teachers', 'admins', ]
+        
+    def get_name(self, obj):
+        return obj.name.title()
+    
+    def get_school_type(self, obj):
+        return obj.school_type.title()
+
+    def get_school_district(self, obj):
+        return obj.school_district.title()
+
+    def get_province(self, obj):
+        return obj.province.title()
+
+    def get_students(self, obj):
+        return CustomUser.objects.filter(role='STUDENT', school=obj).count()
+
+    def get_parents(self, obj):
+        return CustomUser.objects.filter(role='PARENT', school=obj).count()
+
+    def get_teachers(self, obj):
+        return CustomUser.objects.filter(role='TEACHER', school=obj).count()
+
+    def get_admins(self, obj):
+        return CustomUser.objects.filter(Q(role='ADMIN') | Q(role='PRINCIPAL'), school=obj).count()
+        
