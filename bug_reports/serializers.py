f7c4f17b7cab78e427dc2b066df6b9f24a7c6e67
Tsholo Koketso <101592023+Tsholo-K@users.noreply.github.com>
2024-05-23 19:17:39 +0200

    update

--- bug_reports/serializers.py
+++ bug_reports/serializers.py
@@ -5,6 +5,9 @@ import os
 # rest framework
 from rest_framework import serializers
 
+# django
+from django.core.cache import cache
+
 # cryptography
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
@@ -28,14 +31,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # cloudfront url signer 
 def rsa_signer(message):
-    with open(os.path.join(BASE_DIR, 'private_key.pem'), 'rb') as key_file:
+    with open(os.path.join(BASE_DIR, 'private_keys/cloudfront_private_key.pem'), 'rb') as key_file:
         private_key = serialization.load_pem_private_key(
             key_file.read(),
             password=None,
             backend=default_backend()
         )
     return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())
-key_id = 'K1E45RUK43W3WT'
+key_id = 'K2HSBJR82PHOT4' # public keys id
 cloudfront_signer = CloudFrontSigner(key_id, rsa_signer)
 
 
@@ -84,22 +87,47 @@ class BugReportSerializer(serializers.ModelSerializer):
         return obj.status.replace("_", " ").title()
     
     def get_user(self, obj):
+      
         try:
             user = CustomUser.objects.get(pk=obj.user.id)
+   
         except CustomUser.DoesNotExist:
             return None
+  
         if user:
+         
+            # if the user has no profile image return the default profile image 
             if not user.profile_picture:
-                s3_url = 'https://seeran-storage.s3.amazonaws.com/defaults/default-user-icon.svg'
+                s3_url = 'https://seeranbucket.s3.amazonaws.com/defaults/default-user-icon.svg'
+        
+            # if they do have a profile image
             else:
-                s3_url = user.profile_picture.url
-            cloudfront_url = s3_url.replace('https://seeran-storage.s3.amazonaws.com', 'https://d376l49ehaoi1m.cloudfront.net')
+                # try to get the users signed image url from cache
+                s3_url = cache.get(user.email + 'profile_picture')
+                
+                # if its not there get their profile picture url from the db
+                if s3_url == None:
+                    s3_url = user.profile_picture.url
+        
+                # if there's a signed url in the cache return it instead
+                else:
+                    return s3_url
+        
+            # make sure the url format is valid 
+            cloudfront_url = s3_url.replace('https://seeranbucket.s3.amazonaws.com', 'https://d31psdy2k7b4vc.cloudfront.net')
+            
             # Calculate expiration time (current time + 1 hour)
-            expiration_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
+            expiration_time = datetime.datetime.now() + datetime.timedelta(hours=1)
+        
+            # sign the url
             signed_url = cloudfront_signer.generate_presigned_url(
                 cloudfront_url, 
                 date_less_than=expiration_time
             )
+    
+            # save it to cache for an hour
+            cache.set(user.email + 'profile_picture', signed_url, timeout=3600)
+      
             return {
                 "name" : user.name,
                 "surname" : user.surname,
@@ -107,6 +135,7 @@ class BugReportSerializer(serializers.ModelSerializer):
                 'image': signed_url,
                 # add any other fields you want to include
             }
+     
         else:
             return None
         
