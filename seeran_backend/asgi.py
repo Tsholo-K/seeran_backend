4e606e0fa5d4f19e06cea4f5a4fdb7573c23a057
Tsholo Koketso <101592023+Tsholo-K@users.noreply.github.com>
2024-05-29 16:51:20 +0200

    mb

--- seeran_backend/asgi.py
+++ seeran_backend/asgi.py
@@ -3,7 +3,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from messages.routing import websocket_urlpatterns
 from channels.layers import get_channel_layer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seeran_backend.settings')
@@ -14,8 +13,5 @@ channel_layer = get_channel_layer()
 application = ProtocolTypeRouter(
     {
         "http": wsgi_application,  # Regular Django ASGI application for HTTP
-        "websocket": AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)  # WebSocket routing
-        ),
     }
 )
