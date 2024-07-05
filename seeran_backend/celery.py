02a55dc756d238a9717a9307ee1ada2a4493b8fa
Tsholo Koketso <101592023+Tsholo-K@users.noreply.github.com>
2024-07-05 07:09:01 +0200

    yah neh

--- seeran_backend/celery.py
+++ seeran_backend/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seeran_backend.settings')
 
-app = Celery('myproject')
+app = Celery('seeran_backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
