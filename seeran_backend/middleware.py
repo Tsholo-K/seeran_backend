67e9bff22d6461ace50e083c268f1c7893a360c4
Tsholo Koketso <101592023+Tsholo-K@users.noreply.github.com>
2024-07-08 15:34:56 +0000

    update

--- seeran_backend/middleware.py
+++ seeran_backend/middleware.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.cache import cache
 from users.models import CustomUser
 from rest_framework_simplejwt.tokens import AccessToken
-from authentication.utils import validate_access_token, refresh_access_token
+from authentication.utils import validate_access_token
 
 class TokenAuthMiddleware:
     def __init__(self, app):
@@ -17,44 +17,32 @@ class TokenAuthMiddleware:
 
         headers = dict(scope['headers'])
         if b'cookie' in headers:
+            
+            try:
+                cookies = headers[b'cookie'].decode()
+                access_token = cookies.get('access_token')
 
-            cookies = headers[b'cookie'].decode()
-            access_token = cookies.get('access_token')
-            refresh_token = cookies.get('refresh_token')
-
-            if not refresh_token or cache.get(refresh_token):
-                await send({
-                    'type': 'websocket.close',
-                    'code': 1000,
-                    'text': 'Request not authenticated.. access denied'
-                })
-                return
-
-            if not access_token:
-                new_access_token = refresh_access_token(refresh_token)
-                
-            else:
-                new_access_token = validate_access_token(access_token)
-                if new_access_token is None:
-                    new_access_token = refresh_access_token(refresh_token)
-
-            if new_access_token:
-                decoded_token = AccessToken(new_access_token)
-                try:
-                    scope['user'] = await self.get_user(decoded_token['user_id'])
-                except ObjectDoesNotExist:
-                    await send({
-                        'type': 'websocket.close',
-                        'code': 1000,
-                        'text': 'Invalid credentials.. no such user exists'
-                    })
+                if not access_token or cache.get(access_token):
+                    await send({ 'type': 'websocket.close', 'code': 1000, 'text': 'Request not authenticated.. access denied' })
                     return
-            else:
-                await send({
-                    'type': 'websocket.close',
-                    'code': 1000,
-                    'text': 'Invalid security credentials.. request revoked'
-                })
+
+                authorized = validate_access_token(access_token)
+                
+                if authorized is None:
+                    await send({ 'type': 'websocket.close', 'code': 1000, 'text': 'Invalid security credentials.. request revoked' })
+                    return
+
+                decoded_token = AccessToken(access_token)
+                
+                scope['user'] = await self.get_user(decoded_token['user_id'])
+                
+            except ObjectDoesNotExist:
+                await send({ 'type': 'websocket.close', 'code': 1000, 'text': 'Invalid credentials.. no such user exists' })
+                return
+            
+            # if any exception occurs during the proccess return an error
+            except Exception as e:
+                await send({ 'type': 'websocket.close', 'code': 1000, 'text': str(e) })
                 return
 
         return await self.app(scope, receive, send)
