1b42a186dd2db7c43700ea70cd6c99331da31c98
Abraham's descendant <tsholo.koketso@icloud.com>
2024-07-20 06:04:35 +0200

    update

--- seeran_backend/settings.py
+++ seeran_backend/settings.py
@@ -290,7 +290,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC+02:00'
+TIME_ZONE = 'Africa/Johannesburg'
 USE_I18N = True
 USE_TZ = True
 
