32786d78e6167c4ff831362486b110eb7997941e
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-06 00:07:04 +0200

    mb

--- users/consumers/admin/admin_async_functions.py
+++ users/consumers/admin/admin_async_functions.py
@@ -1141,9 +1141,9 @@ def add_students_to_class(user, details):
             classroom.save()
         
         if register:
-            return {'message': f'the provided list of students are now part of the grade {classroom.grade}, group {classroom.group.title()} {classroom.subject.subject.lower()} class.'}
+            return {'message': f'the provided list of students are now part of the grade {classroom.grade.grade}, group {classroom.group.title()} {classroom.subject.subject.lower()} class.'}
         else:
-            return {'message': f'the provided list of students are now part of the grade {classroom.grade}, group {classroom.group.title()} register class.'}
+            return {'message': f'the provided list of students are now part of the grade {classroom.grade.grade}, group {classroom.group.title()} register class.'}
 
     except CustomUser.DoesNotExist:
         # Handle case where the user account does not exist
