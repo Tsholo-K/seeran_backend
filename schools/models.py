9a4af4f08aa3b7489a8f263e490132bbb2890475
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-13 22:09:21 +0200

    mb

--- schools/models.py
+++ schools/models.py
@@ -153,7 +153,7 @@ class Term(models.Model):
         total_weight = Term.objects.filter(school=self.school).exclude(pk=self.pk).aggregate(models.Sum('weight'))['weight__sum'] or 0
 
         # Convert total_weight to Decimal if it's not already
-        total_weight = Decimal(total_weight)
+        total_weight = Decimal(str(total_weight))
 
         if total_weight + self.weight > Decimal('100.00') or total_weight + self.weight < Decimal('0.00'):
             raise ValidationError(_(f'the total weight of all terms aggregated cannot exceed 100% or be less than 0% for any given school year'))
