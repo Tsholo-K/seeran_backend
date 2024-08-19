7c8bf1e128f29edc3f864ee1483f04be0146d358
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-19 15:23:03 +0200

    update

--- users/consumers/admin/admin_async_functions.py
+++ users/consumers/admin/admin_async_functions.py
@@ -1172,11 +1172,11 @@ def form_data_for_creating_class(user, details):
             subject = Subject.objects.get(subject_id=details.get('subject'))
 
             # Retrieve all teachers in the user's school who are not teaching the specified subject
-            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__subject=subject)
+            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__subject=subject).only('account_id', 'name', 'surname', 'email')
 
         elif details.get('reason') == 'register class':
             # Retrieve teachers not currently teaching a register class
-            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__register_class=True)
+            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__register_class=True).only('account_id', 'name', 'surname', 'email')
 
         else:
             return {"error": "Invalid reason provided. Expected 'subject class' or 'register class'."}
@@ -1227,12 +1227,12 @@ def form_data_for_updating_class(user, details):
 
         # Determine the query based on the classroom type
         if classroom.subject:
-            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__subject=classroom.subject)
+            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__subject=classroom.subject).only('account_id', 'name', 'surname', 'email')
             if classroom.teacher:
                 teachers = teachers.exclude(account_id=classroom.teacher.account_id)
 
         elif classroom.register_class:
-            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__register_class=True)
+            teachers = CustomUser.objects.filter(role='TEACHER', school=account.school).exclude(taught_classes__register_class=True).only('account_id', 'name', 'surname', 'email')
             if classroom.teacher:
                 teachers = teachers.exclude(account_id=classroom.teacher.account_id)
 
@@ -1246,6 +1246,75 @@ def form_data_for_updating_class(user, details):
         response_data = {'teacher': AccountSerializer(classroom.teacher).data if classroom.teacher else None, "teachers": teachers, 'group': classroom.group, 'classroom_identifier': classroom.classroom_number}
 
         return response_data
+
+    except CustomUser.DoesNotExist:
+        # Handle case where the user account does not exist
+        return {'error': 'An account with the provided credentials does not exist. Please check the account details and try again.'}
+    
+    except Classroom.DoesNotExist:
+        # Handle case where the classroom does not exist
+        return {'error': 'A classroom with the provided credentials does not exist. Please check the classroom details and try again.'}
+    
+    except Exception as e:
+        # Handle any other unexpected errors
+        return {'error': str(e)}
+
+
+@database_sync_to_async
+def form_data_for_adding_students_to_class(user, details):
+    """
+    Retrieves a list of students who can be added to a specified class based on the provided details.
+    
+    This function handles two main scenarios:
+    1. `subject class`: Fetches students in the same grade who are not enrolled in any class with the same subject as the provided classroom.
+    2. `register class`: Fetches students in the same grade who are not already enrolled in a register class.
+    
+    Args:
+        user (str): The unique identifier (account_id) of the user making the request.
+        details (dict): A dictionary containing details required for fetching the students. It should include:
+            - 'class_id' (str): The ID of the classroom to which students are to be added.
+            - 'reason' (str): The reason for fetching students, which can be 'subject class' or 'register class'.
+    
+    Returns:
+        dict: A dictionary containing:
+            - "students": A serialized list of students who meet the criteria.
+            - "error": An error message if an exception occurs.
+    """
+    try:
+        # Retrieve the account of the user making the request using `select_related` to optimize query performance
+        account = CustomUser.objects.select_related('school').get(account_id=user)
+
+        # Retrieve the classroom with the provided class ID and related data using `select_related`
+        classroom = Classroom.objects.select_related('school', 'grade', 'subject').get(class_id=details.get('class_id'))
+
+        # Check if the user is allowed to access the classroom
+        if account.school != classroom.school:
+            return {"error": "Permission denied. The provided classroom is not from your school. You can only view details about classes from your own school."}
+
+        # Determine the reason for fetching students and apply the appropriate filtering logic
+        if details.get('reason') == 'subject class':
+            # Check if the classroom has a subject linked to it
+            if not classroom.subject:
+                return {"error": "could not proccess your request. the provided classroom has no subject linked to it."}
+
+            # Exclude students who are already enrolled in any class with the same subject as the classroom
+            students = CustomUser.objects.filter(grade=classroom.grade).exclude(enrolled_classes__subject=classroom.subject).only('account_id', 'name', 'surname', 'id_number', 'passport_number', 'email')
+
+        elif details.get('reason') == 'register class':
+            # Check if the classroom is a register class
+            if not classroom.register_class:
+                return {"error": "could not proccess your request. the provided classroom is not a register class."}
+
+            # Exclude students who are already enrolled in a register class in the same grade
+            students = CustomUser.objects.filter(grade=classroom.grade).exclude(enrolled_classes__register_class=True).only('account_id', 'name', 'surname', 'id_number', 'passport_number', 'email')
+
+        else:
+            # Return an error if the reason provided is not valid
+            return {"error": "Invalid reason provided."}
+
+        # Serialize the list of students to return them in the response
+        serializer = AccountSerializer(students, many=True)
+        return {"students": serializer.data}
 
     except CustomUser.DoesNotExist:
         # Handle case where the user account does not exist
@@ -1305,83 +1374,6 @@ def form_data_add_students_to_group_schedule(user, details):
         # Handle case where the group schedule does not exist
         return {'error': 'A group schedule with the provided credentials does not exist. Please check the group schedule details and try again.'}
 
-    except Exception as e:
-        # Handle any other unexpected errors
-        return {'error': str(e)}
-
-    
-
-@database_sync_to_async
-def form_data_for_adding_students_to_class(user, details):
-    """
-    Retrieves a list of students who can be added to a specified class based on the provided details.
-    
-    This function handles two main scenarios:
-    1. `subject class`: Fetches students in the same grade who are not enrolled in any class with the same subject as the provided classroom.
-    2. `register class`: Fetches students in the same grade who are not already enrolled in a register class.
-    
-    Args:
-        user (str): The unique identifier (account_id) of the user making the request.
-        details (dict): A dictionary containing details required for fetching the students. It should include:
-            - 'class_id' (str): The ID of the classroom to which students are to be added.
-            - 'reason' (str): The reason for fetching students, which can be 'subject class' or 'register class'.
-    
-    Returns:
-        dict: A dictionary containing:
-            - "students": A serialized list of students who meet the criteria.
-            - "error": An error message if an exception occurs.
-    """
-    try:
-        # Retrieve the account of the user making the request using `select_related` to optimize query performance
-        account = CustomUser.objects.select_related('school').get(account_id=user)
-
-        # Retrieve the classroom with the provided class ID and related data using `select_related`
-        classroom = Classroom.objects.select_related('school', 'grade', 'subject').get(class_id=details.get('class_id'))
-
-        # Check if the user is allowed to access the classroom
-        if account.school != classroom.school:
-            return {"error": "Permission denied. The provided classroom is not from your school. You can only view details about classes from your own school."}
-
-        # Determine the reason for fetching students and apply the appropriate filtering logic
-        reason = details.get('reason')
-        if reason == 'subject class':
-            # Check if the classroom has a subject linked to it
-            if not classroom.subject:
-                return {"error": "Permission denied. The provided classroom has no subject linked to it."}
-
-            # Get all students in the same grade as the classroom
-            students_in_grade = CustomUser.objects.filter(grade=classroom.grade).only('account_id', 'name', 'surname', 'id_number', 'passport_number', 'email')
-
-            # Exclude students who are already enrolled in any class with the same subject as the classroom
-            students = students_in_grade.exclude(enrolled_classes__subject=classroom.subject)
-
-        elif reason == 'register class':
-            # Check if the classroom is a register class
-            if not classroom.register_class:
-                return {"error": "Permission denied. The provided classroom is not a register class."}
-
-            # Get all students in the same grade as the classroom
-            students_in_grade = CustomUser.objects.filter(grade=classroom.grade).only('account_id', 'name', 'surname', 'id_number', 'passport_number', 'email')
-
-            # Exclude students who are already enrolled in a register class in the same grade
-            students = students_in_grade.exclude(enrolled_classes__register_class=True)
-
-        else:
-            # Return an error if the reason provided is not valid
-            return {"error": "Invalid reason provided."}
-
-        # Serialize the list of students to return them in the response
-        serializer = AccountSerializer(students, many=True)
-        return {"students": serializer.data}
-
-    except CustomUser.DoesNotExist:
-        # Handle case where the user account does not exist
-        return {'error': 'An account with the provided credentials does not exist. Please check the account details and try again.'}
-    
-    except Classroom.DoesNotExist:
-        # Handle case where the classroom does not exist
-        return {'error': 'A classroom with the provided credentials does not exist. Please check the classroom details and try again.'}
-    
     except Exception as e:
         # Handle any other unexpected errors
         return {'error': str(e)}
