91a8b107159fe907e0af94e72b1ced653e5d1be9
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-20 01:53:18 +0200

    no

--- classes/serializers.py
+++ classes/serializers.py
@@ -48,7 +48,7 @@ class ClassSerializer(serializers.ModelSerializer):
         return None
 
     def get_students(self, obj):
-        return AccountSerializer(obj.students.all(), many=True).data
+        return AccountSerializer(obj.students, many=True).data
             
     def get_subject(self, obj):
         if  obj.register_class:
