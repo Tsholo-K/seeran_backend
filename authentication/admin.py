a0be409317cbf1f94f650058a548be0cd0d5e00f
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-24 15:44:59 +0200

    mb

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # models
-from users.models import CustomUser
+from users.models import BaseUser
 
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(BaseUser)
