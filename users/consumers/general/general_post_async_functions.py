26bf6844346897a96cb758b8e75121cec045af21
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-25 14:14:01 +0200

    try

--- users/consumers/general/general_post_async_functions.py
+++ users/consumers/general/general_post_async_functions.py
@@ -46,8 +46,10 @@ def delete_school_account(user, role, details):
         
         elif details.get('school') == 'requesting my own school':
             # Retrieve the user and related school in a single query using select_related
-            admin = Principal.objects.select_related('school').only('school').get(account_id=user)
-            school = admin.school
+            if role == 'PRINCIPAL':
+                admin = Principal.objects.select_related('school').only('school').get(account_id=user)
+
+                school = admin.school
 
         else:
             # Retrieve the School instance by school_id from the provided details
