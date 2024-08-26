4eff19e81dc7db5a701805767c85739393488819
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-26 03:49:53 +0200

    update

--- schools/signals.py
+++ schools/signals.py
@@ -35,30 +35,4 @@ def update_user_counts_on_create(sender, instance, created, **kwargs):
 
         school.save()
 
-@receiver(post_delete, sender=BaseUser)
-def update_user_counts_on_delete(sender, instance, **kwargs):
-    role = instance.role
 
-    role_mapping = {
-        'PRINCIPAL': Principal,
-        'ADMIN': Admin,
-        'TEACHER': Teacher,
-        'STUDENT': Student,
-    }
-
-    if role in role_mapping:
-        # Fetch the corresponding child model and serializer based on the user's role
-        Model = role_mapping[role]
-        account = Model.objects.get(account_id=instance.account_id)
-        school = account.school
-
-        if role in ['PRINCIPAL', 'ADMIN']:
-            school.admin_count = school.principal.count() + school.admins.count()
-        elif role == 'STUDENT':
-            school.student_count = school.students.count()
-        elif role == 'TEACHER':
-            school.teacher_count = school.teachers.count()
-    else:
-        return
-    
-    school.save()
--- users/consumers/admin/admin_post_async_functions.py
+++ users/consumers/admin/admin_post_async_functions.py
@@ -529,7 +529,6 @@ def delete_account(user, role, details):
 
     try:
         child_model_mapping = {
-            'PARENT': (Parent, None, 'children__school'),
             'ADMIN': (Admin, 'school', None),
             'TEACHER': (Teacher, 'school', None),
             'STUDENT': (Student, 'school', None),
@@ -566,7 +565,14 @@ def delete_account(user, role, details):
 
             with transaction.atomic():
                 requested_account.delete()
-                                
+
+                if details['role'] == 'ADMIN':
+                    requesting_account.school.admin_count = requesting_account.school.principal.count() + requesting_account.school.admins.count()
+                elif details['role'] == 'STUDENT':
+                    requesting_account.school.student_count = requesting_account.school.students.count()
+                elif details['role'] == 'TEACHER':
+                    requesting_account.school.teacher_count = requesting_account.school.teachers.count()
+
             return {"message" : 'account successfully deleted'}
         
         return {"error": 'could not proccess your request, the provided account role is invalid'}
