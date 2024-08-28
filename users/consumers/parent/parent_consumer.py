de452557d36d8799de12de8c19ef5072d315a85a
Abraham's descendant <tsholo.koketso@icloud.com>
2024-08-28 05:56:18 +0200

    tf

--- users/consumers/parent/parent_consumer.py
+++ users/consumers/parent/parent_consumer.py
@@ -11,7 +11,6 @@ from seeran_backend.middleware import connection_manager
 from authentication.utils import validate_access_token
 
 # async functions 
-from . import parent_async_functions
 
 # general async functions 
 from users.consumers.general import general_post_async_functions
@@ -23,14 +22,14 @@ from users.consumers.general import general_email_async_functions
 from users.consumers.general import general_form_data_async_functions
 
 
-class ParentConsumer(AsyncWebsocketConsumer):
+class TeacherConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         # Get the user's role from the scope
         role = self.scope.get('role')
 
         # Check if the user has the required role
-        if role not in ['PARENT']:
+        if role not in ['TEACHER']:
             return await self.close()
                 
         account_id = self.scope['user']
@@ -49,6 +48,7 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         user = self.scope.get('user')
+        role = self.scope.get('role')
         access_token = self.scope.get('access_token')
 
         if not (user and access_token and validate_access_token(access_token)):
@@ -63,7 +63,7 @@ class ParentConsumer(AsyncWebsocketConsumer):
         if not action or not description:
             return await self.send(text_data=json.dumps({'error': 'invalid request..'}))
 
-        response = await self.handle_request(action, description, details, user, access_token)
+        response = await self.handle_request(action, description, details, user, role, access_token)
         
         if response is not None:
             return await self.send(text_data=json.dumps(response))
@@ -72,7 +72,7 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
 
 
-    async def handle_request(self, action, description, details, user, access_token):
+    async def handle_request(self, action, description, details, user, role, access_token):
         action_map = {
             'GET': self.handle_get,
             'SEARCH': self.handle_search,
@@ -84,15 +84,15 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
         handler = action_map.get(action)
         if handler:
-            return await handler(description, details, user, access_token)
+            return await handler(description, details, user, role, access_token)
         
         return {'error': 'Invalid action'}
 
 
 
-    async def handle_get(self, description, details, user, access_token):
+    async def handle_get(self, description, details, user, role, access_token):
         get_map = {
-            'my_security_information': general_get_async_functions.fetch_my_security_information,
+            'my_security_information': general_get_async_functions.fetch_security_information,
             'email_information': general_get_async_functions.fetch_my_email_information,
 
             'chats': general_get_async_functions.fetch_chats,
@@ -102,43 +102,45 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
         func = get_map.get(description)
         if func:
-            return await func(user) if description != 'log_out' else await func(access_token)
+            return await func(user) if description in ['chats', 'email_information'] else await func(user, role)
         
         return {'error': 'Invalid get description'}
 
 
 
-    async def handle_search(self, description, details, user, access_token):
+    async def handle_search(self, description, details, user, role, access_token):
         search_map = {
+            'account': general_search_async_functions.search_account,
+            
             'parents': general_search_async_functions.search_parents,
-
-            'account_profile': general_search_async_functions.search_account_profile,
-            'account_id': general_search_async_functions.search_account_id,
 
             'group_schedule_schedules': general_search_async_functions.search_group_schedule_schedules,
             'group_schedules': general_search_async_functions.search_group_schedules,
-            'schedule_sessions': general_search_async_functions.search_for_schedule_sessions,
 
-            'month_attendance_records': general_search_async_functions.search_month_attendance_records,
+            'schedule_sessions': general_search_async_functions.search_schedule_sessions,
 
             'announcement': general_search_async_functions.search_announcement,
 
             'chat_room': general_search_async_functions.search_chat_room,
             'chat_room_messages': general_search_async_functions.search_chat_room_messages,
 
-            'class': general_search_async_functions.search_class,
-            'student_class_card': general_search_async_functions.search_student_class_card,
-
             'activity': general_search_async_functions.search_activity,
 
-            'email_ban': general_search_async_functions.search_my_email_ban,
+            'email_ban': general_search_async_functions.search_email_ban,
         }
 
         func = search_map.get(description)
         
         if func:
-            response = await func(details) if description in ['schedule_sessions'] else await func(user, details)
+            if description in ['schedule_sessions', 'email_ban']:
+                response = await func(details) 
             
+            elif description in ['chat_room_messages']:
+                response = await func(user, details)
+            
+            else:
+                response =  await func(user, role, details)
+                        
             if response.get('user') and description in ['chat_room_messages']:
                 await connection_manager.send_message(response['user'], json.dumps({'description': 'read_receipt', 'chat': response['chat']}))
                 await connection_manager.send_message(user, json.dumps({'unread_messages': response['unread_messages']} ))
@@ -150,7 +152,7 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
 
 
-    async def handle_verify(self, description, details, user, access_token):
+    async def handle_verify(self, description, details, user, role, access_token):
         verify_map = {
             'verify_email': general_verify_async_functions.verify_email,
             'verify_password': general_verify_async_functions.verify_password,
@@ -158,8 +160,8 @@ class ParentConsumer(AsyncWebsocketConsumer):
             'verify_otp': general_verify_async_functions.verify_otp,
             
             'verify_email_revalidation_otp': general_verify_async_functions.verify_email_revalidate_otp,
-            'send_email_revalidation_otp': general_verify_async_functions.validate_email_revalidation,
 
+            'send_email_revalidation_otp': general_verify_async_functions.validate_email_revalidation,
         }
 
         func = verify_map.get(description)
@@ -176,20 +178,20 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
 
 
-    async def handle_form_data(self, description, details, user, access_token):
+    async def handle_form_data(self, description, details, user, role, access_token):
         form_data_map = {
         }
 
         func = form_data_map.get(description)
         if func:
-            response = await func(user, details)
+            response = await func(user, role, details)
             return response
         
         return {'error': 'Invalid form data description'}
 
 
 
-    async def handle_put(self, description, details, user, access_token):
+    async def handle_put(self, description, details, user, role, access_token):
         put_map = {
             'update_email': general_put_async_functions.update_email,
             'update_password': general_put_async_functions.update_password,
@@ -201,18 +203,25 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
         func = put_map.get(description)
         if func:
-            response = await func(user, details, access_token) if description in ['update_email', 'update_password'] else await func(user, details)
+            if description in ['update_email', 'update_password']:
+                response = await func(user, role, details, access_token)
+
+            elif description in ['update_multi_factor_authentication']:
+                response = await func(user, details)
+
+            else:
+                response = await func(user, role, details)
                         
-            if response.get('user') and description in ['mark_messages_as_read']:
-                await connection_manager.send_message(response['user'], json.dumps({'description': 'read_receipt', 'chat': response['chat']}))
+            if response.get('user'):
+                if description in ['mark_messages_as_read']:
+                    await connection_manager.send_message(response['user'], json.dumps({'description': 'read_receipt', 'chat': response['chat']}))
+                    return {'message': 'read receipt sent'}
 
-                return {'message': 'read receipt sent'}
+                elif description in ['send_email_revalidation_otp']:
+                    response = await general_email_async_functions.send_email_revalidation_one_time_pin_email(response['user'])
 
-            if description == 'send_email_revalidation_otp' and response.get('user'):
-                response = await general_email_async_functions.send_email_revalidation_one_time_pin_email(response.get('user'))
-
-                if response.get('message'):
-                    return await general_put_async_functions.update_email_ban_otp_sends(details)
+                    if response.get('message'):
+                        return await general_put_async_functions.update_email_ban_otp_sends(details)
                 
             return response
         
@@ -220,21 +229,21 @@ class ParentConsumer(AsyncWebsocketConsumer):
 
 
 
-    async def handle_post(self, description, details, user, access_token):
+    async def handle_post(self, description, details, user, role, access_token):
         post_map = {
             'text': general_post_async_functions.text,
-
+            
             'log_out': general_post_async_functions.log_out,
         }
 
         func = post_map.get(description)
 
         if func:
-            response = await func(access_token) if description in ['log_out'] else func(user, details)
+            response = await func(access_token) if description in ['log_out'] else await func(user, role, details)
             
             if response.get('reciever') and description in ['text']:
-                await connection_manager.send_message(response['reciever']['id'], json.dumps({'description': 'text_message', 'message': response['message'], 'sender': response['sender']}))
-                await connection_manager.send_message(response['sender']['id'], json.dumps({'description': 'text_message_fan', 'message': response['message'], 'reciever': response['reciever']}))
+                await connection_manager.send_message(response['reciever']['account_id'], json.dumps({'description': 'text_message', 'message': response['message'], 'sender': response['sender']}))
+                await connection_manager.send_message(response['sender']['account_id'], json.dumps({'description': 'text_message_fan', 'message': response['message'], 'reciever': response['reciever']}))
 
                 return {'message': 'text message sent'}
 
