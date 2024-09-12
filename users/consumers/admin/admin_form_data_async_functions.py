d128efbceb4e4c6cc18aa0f47c6f21493fca4e9d
Abraham's descendant <tsholo.koketso@icloud.com>
2024-09-12 23:55:25 +0200

    update

--- users/consumers/admin/admin_form_data_async_functions.py
+++ users/consumers/admin/admin_form_data_async_functions.py
@@ -261,10 +261,10 @@ def form_data_for_collecting_assessment_submittions(user, role, details):
 
         if assessment.classroom:
             # Fetch students in the classroom who haven't submitted
-            students = assessment.classroom.students.filter(search_filters).only('name', 'surname', 'id_number', 'passport_number', 'account_id', 'profile_picture').exclude(account_id__in=submitted_student_ids).order_by('id')[:20]
+            students = assessment.classroom.students.filter(search_filters).only('name', 'surname', 'id_number', 'passport_number', 'account_id', 'profile_picture').exclude(account_id__in=submitted_student_ids).order_by('id')[:2]
         elif assessment.grade:
             # Fetch students in the grade who haven't submitted
-            students = assessment.grade.students.filter(search_filters).only('name', 'surname', 'id_number', 'passport_number', 'account_id', 'profile_picture').exclude(account_id__in=submitted_student_ids).order_by('id')[:20]
+            students = assessment.grade.students.filter(search_filters).only('name', 'surname', 'id_number', 'passport_number', 'account_id', 'profile_picture').exclude(account_id__in=submitted_student_ids).order_by('id')[:2]
 
         else:
             return {'error': 'No valid classroom or grade found for the assessment.'}
@@ -274,7 +274,7 @@ def form_data_for_collecting_assessment_submittions(user, role, details):
         # Determine the next cursor (based on the primary key)
         next_cursor = students[-1].id if len(students) == 20 else None
 
-        return {'students': serialized_students, 'next_cursor': next_cursor}
+        return {'students': serialized_students, 'cursor': next_cursor}
 
     except Assessment.DoesNotExist:
         return {'error': 'No assessment found with the provided details in your school.'}
