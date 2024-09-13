678f0e1bf36ce3c7b17bd6f1b400fcb1ed788f1a
Abraham's descendant <tsholo.koketso@icloud.com>
2024-09-13 12:21:17 +0200

    mb

--- users/consumers/admin/admin_form_data_async_functions.py
+++ users/consumers/admin/admin_form_data_async_functions.py
@@ -293,63 +293,6 @@ def form_data_for_collecting_assessment_submissions(user, role, details):
 
 
 @database_sync_to_async
-def form_data_for_collecting_assessment_submittions(user, role, details):
-    try:
-        # Retrieve the requesting user's account and related school in a single query using select_related
-        requesting_account = users_utilities.get_account_and_linked_school(user, role)
-
-        # Check if the user has permission to collect assessment submissions
-        if role != 'PRINCIPAL' and not permissions_utilities.has_permission(requesting_account, 'COLLECT', 'ASSESSMENT'):
-            response = 'You do not have the necessary permissions to collect assessment submissions.'
-            audits_utilities.log_audit(actor=requesting_account, action='COLLECT', target_model='ASSESSMENT', outcome='DENIED', response=response, school=requesting_account.school)
-            return {'error': response}
-
-        # Fetch the assessment from the requesting user's school
-        assessment = requesting_account.school.assessments.select_related('classroom', 'grade').get(assessment_id=details.get('assessment'))
-
-        # Get the list of students who have already submitted the assessment
-        submitted_student_ids = assessment.submissions.values_list('student__account_id', flat=True)
-
-        search_filters = Q()
-
-        # Apply search filters if provided
-        if 'search_query' in details:
-            search_query = details.get('search_query')
-            search_filters &= (Q(name__icontains=search_query) | Q(surname__icontains=search_query) | Q(account_id__icontains=search_query))
-
-        # Apply cursor for pagination using the primary key (id)
-        if 'cursor' in details and details['cursor'] is not None:
-            cursor = details.get('cursor')
-            search_filters &= Q(id__gt=cursor)
-
-        if assessment.classroom:
-            # Fetch students in the classroom who haven't submitted
-            students = assessment.classroom.students.filter(search_filters).only('name', 'surname', 'id_number', 'passport_number', 'account_id', 'profile_picture').exclude(account_id__in=submitted_student_ids).order_by('id')[:4]
-        elif assessment.grade:
-            # Fetch students in the grade who haven't submitted
-            students = assessment.grade.students.filter(search_filters).only('name', 'surname', 'id_number', 'passport_number', 'account_id', 'profile_picture').exclude(account_id__in=submitted_student_ids).order_by('id')[:4]
-
-        else:
-            return {'error': 'No valid classroom or grade found for the assessment.'}
-        
-        if not students:
-            return {'students': [], 'cursor': None}
-        
-        serialized_students = StudentSourceAccountSerializer(students, many=True).data
-
-        # Determine the next cursor (based on the primary key)
-        next_cursor = students[len(students) - 1].id if students and len(students) > 3 else None
-
-        return {'students': serialized_students, 'cursor': next_cursor}
-
-    except Assessment.DoesNotExist:
-        return {'error': 'No assessment found with the provided details in your school.'}
-
-    except Exception as e:
-        return {'error': str(e)}
-
-
-@database_sync_to_async
 def form_data_for_adding_students_to_group_schedule(user, role, details):
     try:
         # Retrieve the requesting users account and related school in a single query using select_related
