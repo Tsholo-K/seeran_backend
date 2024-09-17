ced5f01be096c9e2c800e6e9c935c342d3704037
Abraham's descendant <tsholo.koketso@icloud.com>
2024-09-17 18:30:33 +0200

    update

--- seeran_backend/celery.py
+++ seeran_backend/celery.py
@@ -28,3 +28,12 @@ app.conf.update(
         'ssl_cert_reqs': ssl.CERT_REQUIRED,  # Ensure SSL certificate is required
     },
 )
+
+app.conf.update(
+    broker_url='rediss://' + config('CACHE_LOCATION') + ':6378',
+    result_backend=None,  # Do not store task results
+    broker_transport_options={
+        'ssl_cert_reqs': ssl.CERT_REQUIRED,  # Ensure SSL certificate is required
+        'ssl_ca_certs': config('SERVER_CA_CERT'),  # Path to CA certificates
+    },
+)
