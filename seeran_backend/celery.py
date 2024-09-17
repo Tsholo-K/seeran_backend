a25b32100c11ae0646a98ba18bbc64fc1fc43756
Abraham's descendant <tsholo.koketso@icloud.com>
2024-09-17 20:16:02 +0200

    retry

--- seeran_backend/celery.py
+++ seeran_backend/celery.py
@@ -3,13 +3,12 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 # celery
-from celery import Celery
-
+import celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seeran_backend.settings')
 
-app = Celery('seeran_backend')
+app = celery.Celery('seeran_backend')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
