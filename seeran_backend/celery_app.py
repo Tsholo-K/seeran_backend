1430a27df8d6011212cd11d6f0ffbcdb9148d014
Abraham's descendant <tsholo.koketso@icloud.com>
2024-09-26 21:25:52 +0200

    update

--- seeran_backend/celery_app.py
+++ seeran_backend/celery_app.py
@@ -11,6 +11,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seeran_backend.settings')
 
 app = Celery('seeran_backend')
 
+app.conf.update(
+    # Other configuration settings
+    worker_log_format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    worker_task_log_format='%(asctime)s - %(name)s - %(levelname)s - %(task_name)s[%(task_id)s]: %(message)s',
+    worker_log_color=False,  # Disable colors if you are writing to a log file
+    worker_redirect_stdouts_level='DEBUG',  # Set logging level to DEBUG
+)
+
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
