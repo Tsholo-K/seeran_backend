73a113441b223fbaea3ac6c6a9df514f3d7ce604
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-03 14:28:05 +0200

    mb

--- websockets/consumers/teacher/teacher_view_async_functions.py
+++ websockets/consumers/teacher/teacher_view_async_functions.py
@@ -58,7 +58,7 @@ def view_my_classrooms(account, role):
             audits_utilities.log_audit(actor=requesting_account, action='VIEW', target_model='CLASSROOM', outcome='DENIED', server_response=response, school=requesting_account.school)
             return {'error': response}
 
-        my_classrooms = requesting_account.taught_classrooms.exclude(register_clasroom=True)
+        my_classrooms = requesting_account.taught_classrooms.exclude(register_classroom=True)
         serialized_classrooms = TeacherClassroomsSerializer(my_classrooms, many=True).data
 
         return {"classrooms": serialized_classrooms}
