ecdb936c08ee9157c1054a245c9bb6f69b715fee
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-04 13:55:30 +0200

    update

--- websockets/consumers/admin/admin_search_async_functions.py
+++ websockets/consumers/admin/admin_search_async_functions.py
@@ -462,7 +462,7 @@ def search_grades(user, role, details):
 
         if details.get('timestamp'):
             # Filter grades created after the given timestamp
-            grades = requesting_account.school.grades.filter(models.Q(last_updated__gt=details['timestamp']) | models.Q(timestamp__gt=details['timestamp']))
+            grades = requesting_account.school.grades.filter(models.Q(last_updated__gt=details['timestamp']))
 
         else:
             grades = requesting_account.school.grades
