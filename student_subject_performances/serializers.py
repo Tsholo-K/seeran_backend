19066abdb1915cde9c871b44a381c201ee65e971
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-08 22:11:50 +0200

    mb

--- student_subject_performances/serializers.py
+++ student_subject_performances/serializers.py
@@ -17,7 +17,7 @@ class StudentPerformanceSerializer(serializers.ModelSerializer):
         fields = ['pass_rate', 'highest_score', 'lowest_score', 'average_score', 'median_score', 'completion_rate', 'mode_score', 'passed', 'assessments']
 
     def get_assessments(self, obj):
-        return GradedAssessmentsSerializer(obj.subject.assessments.filter(term=obj.term, grade=obj.grade, formal=True)).data if obj.subject else None
+        return GradedAssessmentsSerializer(obj.subject.assessments.filter(term=obj.term, grade=obj.grade, formal=True), many=True).data if obj.subject else None
 
 
 
