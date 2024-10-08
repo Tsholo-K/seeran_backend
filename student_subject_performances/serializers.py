e80feec44dcbdfee6b3fbabc418c6e1d872eafce
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-08 22:08:46 +0200

    updates

--- student_subject_performances/serializers.py
+++ student_subject_performances/serializers.py
@@ -4,11 +4,20 @@ from rest_framework import serializers
 # models
 from .models import StudentSubjectPerformance
 
+# serializers
+from assessments.serializers import GradedAssessmentsSerializer
+
 
 class StudentPerformanceSerializer(serializers.ModelSerializer):
 
+    assessments = serializers.SerializerMethodField()
+
     class Meta:
         model = StudentSubjectPerformance
-        fields = ['pass_rate', 'highest_score', 'lowest_score', 'average_score', 'median_score', 'completion_rate', 'mode_score', 'passed']
+        fields = ['pass_rate', 'highest_score', 'lowest_score', 'average_score', 'median_score', 'completion_rate', 'mode_score', 'passed', 'assessments']
+
+    def get_assessments(self, obj):
+        return GradedAssessmentsSerializer(obj.subject.assessments.filter(term=obj.term, grade=obj.grade, formal=True)).data if obj.subject else None
+
 
 
