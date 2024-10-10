95661197e17b4aa59889ffed8c8e59816f26c51e
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-10 18:20:55 +0200

    retry

--- websockets/consumers/admin/admin_search_async_functions.py
+++ websockets/consumers/admin/admin_search_async_functions.py
@@ -1486,7 +1486,9 @@ def search_student_attendance(account, role, details):
         student = classroom.students.get(account_id=details['student'])
 
         # Query for the Absent instances where absentes is True
-        attendances = classroom.attendances.filter(models.Q(absent_students=student) & models.Q(late_students=student) & models.Q(absentes=True))
+        attendances = []
+        attendances.append(student.absences)
+        attendances.append(student.late_arrivals)
 
         # For each absent instance, get the corresponding Late instance
         attendance_records = StudentAttendanceSerializer(attendances, many=True, context={'student': account}).data
