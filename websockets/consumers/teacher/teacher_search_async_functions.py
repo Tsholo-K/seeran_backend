65513672613714da6882e0b04b268f32b5ef270e
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-10 14:24:21 +0200

    update

--- websockets/consumers/teacher/teacher_search_async_functions.py
+++ websockets/consumers/teacher/teacher_search_async_functions.py
@@ -278,49 +278,47 @@ def search_month_attendance_records(account, role, details):
     try:
         # Retrieve the requesting users account and related school in a single query using select_related
         requesting_account = accounts_utilities.get_account_and_linked_school(account, role)
-
+ 
         if not permissions_utilities.has_permission(requesting_account, 'VIEW', 'ATTENDANCE'):
-            response = f'could not proccess your request, you do not have the necessary permissions to view attendances. please contact your administrator to adjust you permissions for viewing attendance details.'
+            response = f'could not proccess your request, you do not have the necessary permissions to view classrooms. please contact your administrator to adjust you permissions for viewing classrooms.'
             audits_utilities.log_audit(actor=requesting_account, action='VIEW', target_model='ATTENDANCE', outcome='DENIED', server_response=response, school=requesting_account.school)
+
             return {'error': response}
 
-        if not 'classroom' in details:
-            response = f'could not proccess your request, the provided information is invalid for the action you are trying to perform. please make sure to provide valid classroom ID and try again'
+        if not {'month_name', 'classroom'}.issubset(details):
+            response = f'could not proccess your request, the provided information is invalid for the action you are trying to perform. please make sure to provide a valid month name and classroom ID and try again'
             audits_utilities.log_audit(actor=requesting_account, action='VIEW', target_model='ATTENDANCE', outcome='ERROR', server_response=response, school=requesting_account.school)
             return {'error': response}
 
-        # Fetch the specific classroom based on class_id and school
-        classroom = requesting_account.taught_classrooms.filter(register_class=True).first()
-        if not classroom:
-            return {"error": _("could not proccess your request, you don\'t seem to be assigned a register classroom")}
-        
-        start_date, end_date = attendances_utilities.get_month_dates(details.get('month_name'))
+        classroom = requesting_account.taught_classrooms.get(classroom_id=details['classroom'], register_classroom=True)
+        start_date, end_date = attendances_utilities.get_month_dates(details['month_name'])
 
         # Query for the Absent instances where absentes is True
-        absents = ClassroomAttendanceRegister.objects.prefetch_related('absent_students').filter(models.Q(date__gte=start_date) & models.Q(date__lt=end_date) & models.Q(classroom=classroom) & models.Q(absentes=True))
+        attendances = classroom.attendances.prefetch_related('absent_students', 'late_students').filter(models.Q(timestamp__gte=start_date) & models.Q(timestamp__lt=end_date) & models.Q(absentes=True))
 
         # For each absent instance, get the corresponding Late instance
         attendance_records = []
-        for absent in absents:
-            late = ClassroomAttendanceRegister.objects.prefetch_related('late_students').filter(date__date=absent.date.date(), classroom=classroom).first()
+        for attendance in attendances:
             record = {
-                'date': absent.date.isoformat(),
-                'absent_students': LeastAccountDetailsSerializer(absent.absent_students.all(), many=True).data,
-                'late_students': LeastAccountDetailsSerializer(late.late_students.all(), many=True).data if late else [],
+                'date': attendance.timestamp.isoformat(),
+                'absent_students': LeastAccountDetailsSerializer(attendance.absent_students, many=True).data if attendance.absent_students else [],
+                'late_students': LeastAccountDetailsSerializer(attendance.late_students, many=True).data if attendance.late_students else [],
             }
             attendance_records.append(record)
 
-        return {'records': attendance_records}
-               
-    except Teacher.DoesNotExist:
-        # Handle the case where the requested teacher account does not exist.
-        return {'error': 'A teacher account with the provided credentials does not exist, please check the account details and try again'}
+        # Compress the serialized data
+        compressed_attendance_records = zlib.compress(json.dumps(attendance_records).encode('utf-8'))
+
+        # Encode compressed data as base64 for safe transport
+        encoded_attendance_records = base64.b64encode(compressed_attendance_records).decode('utf-8')
+
+        return {'records': encoded_attendance_records}
     
     except Classroom.DoesNotExist:
-        return { 'error': 'a classroom in your school with the provided credentials does not exist' }
+        return {'error': 'a classroom in your school with the provided credentials does not exist. Please check the classrooms details and try again.'}
 
     except Exception as e:
-        return { 'error': str(e) }
+        return {'error': str(e)}
 
 
 @database_sync_to_async
