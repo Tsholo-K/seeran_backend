a200552add1b216b96b8b1304a1e49a3ad529a0d
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-13 18:20:32 +0200

    mb

--- websockets/consumers/admin/admin_link_async_functions.py
+++ websockets/consumers/admin/admin_link_async_functions.py
@@ -18,17 +18,16 @@ from audit_logs import utils as audits_utilities
 
 
 @database_sync_to_async
-def link_parent(user, role, details):
+def link_parent(account, role, details):
     try:    
         student = None  # Initialize student as None to prevent issues in error handling
         # Retrieve the requesting users account and related school in a single query using select_related
-        requesting_account = accounts_utilities.get_account_and_linked_school(user, role)
+        requesting_account = accounts_utilities.get_account_and_linked_school(account, role)
 
         # Check if the user has permission to link acounts
         if role != 'PRINCIPAL' and not permissions_utilities.has_permission(requesting_account, 'LINK', 'ACCOUNT'):
             response = f'could not proccess your request, you do not have the necessary permissions to link parents to student accounts'
-            audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', outcome='DENIED', response=response, school=requesting_account.school)
-
+            audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', outcome='DENIED', server_response=response, school=requesting_account.school)
             return {'error': response}
 
         student = Student.objects.prefetch_related('parents').get(account_id=details.get('student'), school=requesting_account.school)
@@ -37,8 +36,7 @@ def link_parent(user, role, details):
         student_parent_count = student.parents.count()
         if student_parent_count >= 2:
             response = f"the provided student account has reached the maximum number of linked parents. please unlink one of the existing parents to link a new one"
-            audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', outcome='DENIED', response=response, school=requesting_account.school)
-
+            audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', outcome='DENIED', server_response=response, school=requesting_account.school)
             return {'error': response}
         
         # Check if an account with the provided email already exists
@@ -55,13 +53,13 @@ def link_parent(user, role, details):
                 parent.children.add(student)
 
                 response = f'parent account successfully created and linked to student. the parent can now sign-in and activate their account'
-                audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='LINKED', response=response, school=requesting_account.school,)
+                audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='LINKED', server_response=response, school=requesting_account.school,)
 
             return {'user' : parent}
             
         # Return serializer errors if the data is not valid, format it as a string
         error_response = '; '.join([f"{key}: {', '.join(value)}" for key, value in serializer.errors.items()])
-        audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='ERROR', response=f'Validation failed: {error_response}', school=requesting_account.school)
+        audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='ERROR', server_response=f'Validation failed: {error_response}', school=requesting_account.school)
 
         return {"error": error_response}
                                       
@@ -71,13 +69,13 @@ def link_parent(user, role, details):
 
     except ValidationError as e:
         error_message = e.messages[0].lower() if isinstance(e.messages, list) and e.messages else str(e).lower()
-        audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='ERROR', response=error_message, school=requesting_account.school)
+        audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='ERROR', server_response=error_message, school=requesting_account.school)
 
         return {"error": error_message}
 
     except Exception as e:
         error_message = str(e)
-        audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='ERROR', response=error_message, school=requesting_account.school)
+        audits_utilities.log_audit(actor=requesting_account, action='LINK', target_model='ACCOUNT', target_object_id=str(student.account_id) if student else 'N/A', outcome='ERROR', server_response=error_message, school=requesting_account.school)
 
         return {'error': error_message}
 
