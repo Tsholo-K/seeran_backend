ee4798cbf433bccee662f1edb42b7c210df6b12d
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-17 18:35:19 +0200

    retry

--- websockets/consumers/general/general_message_async_functions.py
+++ websockets/consumers/general/general_message_async_functions.py
@@ -28,7 +28,10 @@ def message_private(account, role, details):
         # Validate users
         if account == details.get('account'):
             return {"error": "validation error. you can not send a text message to yourself, it violates database constraints and is therefore not allowed."}
-        
+
+        # Retrieve the requested user's account
+        requesting_user = BaseAccount.objects.get(account_id=account)
+
         # Retrieve the requesting users account and related school in a single query using select_related
         requesting_account = accounts_utilities.get_account_and_permission_check_attr(account, role)
 
@@ -47,28 +50,28 @@ def message_private(account, role, details):
             timestamp = timezone.now()
 
             # Retrieve or create the chat room
-            private_chat_room, created = PrivateChatRoom.objects.get_or_create(participant_one=requesting_account if requesting_account.id < requested_user.id else requested_user, participant_two=requested_user if requesting_account.id < requested_user.id else requesting_account, defaults={'participant_one': requesting_account, 'participant_two': requested_user, 'latest_message_timestamp': timestamp})
+            private_chat_room, created = PrivateChatRoom.objects.get_or_create(participant_one=requesting_user if requesting_user.id < requested_user.id else requested_user, participant_two=requested_user if requesting_user.id < requested_user.id else requesting_user, defaults={'participant_one': requesting_user, 'participant_two': requested_user, 'latest_message_timestamp': timestamp})
             
             if created:
                 # Create the new message
-                new_private_chat_room_message = PrivateMessage.objects.create(author=requesting_account, message_content=details.get('message'), chat_room=private_chat_room, timestamp=timestamp)
+                new_private_chat_room_message = PrivateMessage.objects.create(author=requesting_user, message_content=details.get('message'), chat_room=private_chat_room, timestamp=timestamp)
             
             else:
                 # Create the new message
-                new_private_chat_room_message = PrivateMessage.objects.create(author=requesting_account, message_content=details.get('message'), chat_room=private_chat_room)
+                new_private_chat_room_message = PrivateMessage.objects.create(author=requesting_user, message_content=details.get('message'), chat_room=private_chat_room)
 
                 # Retrieve the last message in the chat room
                 private_chat_room_last_message = private_chat_room.messages.order_by('-timestamp').first()
 
                 # Update the last message's 'last' field if it's from the same sender
-                if private_chat_room_last_message and private_chat_room_last_message.author == requesting_account:
+                if private_chat_room_last_message and private_chat_room_last_message.author == requesting_user:
                     private_chat_room_last_message.last_message = False
                     private_chat_room_last_message.save()
 
         # Serialize the new message
         serialized_message = PrivateChatRoomMessageSerializer(new_private_chat_room_message, context={'participant': account}).data
 
-        serialized_author = BareAccountDetailsSerializer(requesting_account).data
+        serialized_author = BareAccountDetailsSerializer(requesting_user).data
         serialized_recipient = BareAccountDetailsSerializer(requested_user).data
 
         return {'message': serialized_message, 'author': serialized_author, 'recipient': serialized_recipient}
