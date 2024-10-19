14057d2f5489a4715171125182266198b98f0ba3
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-19 14:44:28 +0200

    updates

--- chat_room_messages/models.py
+++ chat_room_messages/models.py
@@ -5,13 +5,14 @@ import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 
 class PrivateMessage(models.Model):
     chat_room = models.ForeignKey('chat_rooms.PrivateChatRoom', on_delete=models.CASCADE, related_name='messages')
     
     author = models.ForeignKey('accounts.BaseAccount', on_delete=models.DO_NOTHING)
-    message_content = models.TextField()
+    message_content = models.TextField(max_length=1024)
 
     last_message = models.BooleanField(default=True)
 
@@ -28,3 +29,18 @@ class PrivateMessage(models.Model):
         self.chat_room.save(update_fields=['latest_message_timestamp'])
 
         super().save(*args, **kwargs)
+
+    MAX_MESSAGE_LENGTH = 1024
+
+    # Custom validation method
+    def clean(self):
+        # Trim leading and trailing whitespaces
+        self.message_content = self.message_content.strip()
+
+        # No empty or whitespace-only messages
+        if not self.message_content:
+            raise ValidationError('Message cannot be empty or contain whitespace only.')
+
+        # Maximum length validation
+        if len(self.message_content) > self.MAX_MESSAGE_LENGTH:
+            raise ValidationError(f'Message cannot exceed {self.MAX_MESSAGE_LENGTH} characters.')
