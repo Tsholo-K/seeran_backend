8efadbedca57055a792d3be910089a5c2a7f8e4e
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-20 17:55:23 +0200

    update

--- accounts/serializers/principals/serializers.py
+++ accounts/serializers/principals/serializers.py
@@ -93,9 +93,9 @@ class PrincipalAccountSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         if obj.profile_picture:
-            existing_signed_url = cache.get(str(obj.account_id) + 'profile_picture')
-            if existing_signed_url:
-                return existing_signed_url
+            # existing_signed_url = cache.get(str(obj.account_id) + 'profile_picture')
+            # if existing_signed_url:
+            #     return existing_signed_url
             
             singed_url = accounts_utilities.generate_signed_url(obj.profile_picture.name)
             cache.set(str(obj.account_id) + 'profile_picture', singed_url, timeout=3600) 
@@ -131,9 +131,9 @@ class PrincipalAccountDetailsSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         if obj.profile_picture:
-            existing_signed_url = cache.get(str(obj.account_id) + 'profile_picture')
-            if existing_signed_url:
-                return existing_signed_url
+            # existing_signed_url = cache.get(str(obj.account_id) + 'profile_picture')
+            # if existing_signed_url:
+            #     return existing_signed_url
             
             singed_url = accounts_utilities.generate_signed_url(obj.profile_picture.name)
             cache.set(str(obj.account_id) + 'profile_picture', singed_url, timeout=3600) 
