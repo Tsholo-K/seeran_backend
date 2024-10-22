ee50d0055b9c174a4964498edfd473b1affb8d93
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-22 17:23:22 +0200

    mb

--- accounts/serializers/founders/serializers.py
+++ accounts/serializers/founders/serializers.py
@@ -1,8 +1,14 @@
 # rest framework
 from rest_framework import serializers
 
+# django
+from django.core.cache import cache
+
 # models
 from accounts.models import Founder
+
+# utility functions 
+from accounts import utils as accounts_utilities
 
 
 class FounderAccountDetailsSerializer(serializers.ModelSerializer):
@@ -13,9 +19,19 @@ class FounderAccountDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Founder
         fields = ['name', 'surname', 'role', 'image', 'identifier', 'account_id']
-            
+
     def get_image(self, obj):
-        return obj.profile_picture.url if obj.profile_picture else '/default-user-icon.svg'
+        if obj.profile_picture:
+            existing_signed_url = cache.get(str(obj.account_id) + 'profile_picture')
+            if existing_signed_url:
+                return existing_signed_url
+            
+            singed_url = accounts_utilities.generate_signed_url(obj.profile_picture.name)
+            cache.set(str(obj.account_id) + 'profile_picture', singed_url, timeout=3600) 
+
+            return singed_url
+
+        return '/default-user-icon.svg'
     
     def get_identifier(self, obj):
         return obj.email_address
