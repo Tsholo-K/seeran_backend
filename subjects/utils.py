3aa197d89352a3c2393ee780197ff667eb8ad309
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-28 19:16:30 +0200

    mb

--- subjects/utils.py
+++ subjects/utils.py
@@ -4,10 +4,10 @@ from django.db import models
 def update_subject_counts(subject):
     grade = subject.grade
         
-    subject.classrooms_count = grade.classrooms.filter(subject=subject).count()
+    subject.classroom_count = grade.classrooms.filter(subject=subject).count()
     subject.teacher_count = grade.classrooms.filter(subject=subject).exclude(teacher=None).values_list('teacher', flat=True).distinct().count()
     subject.student_count = grade.classrooms.filter(subject=subject).aggregate(student_count=models.Count('students'))['student_count'] or 0
-    subject.save(update_fields=['classrooms_count', 'teacher_count', 'student_count'])
+    subject.save(update_fields=['classroom_count', 'teacher_count', 'student_count'])
 
 
 def update_subject_role_counts(subject, role):
@@ -24,8 +24,8 @@ def update_subject_role_counts(subject, role):
 def update_subject_classrooms_count(subject):
     grade = subject.grade
         
-    subject.classrooms_count = grade.classrooms.filter(subject=subject).count()
-    subject.save(update_fields=['classrooms_count'])
+    subject.classroom_count = grade.classrooms.filter(subject=subject).count()
+    subject.save(update_fields=['classroom_count'])
 
 
 def update_subject_teacher_count(subject):
