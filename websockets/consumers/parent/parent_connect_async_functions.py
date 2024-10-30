b7d3988b456f1696f47f462561e2c34582b30f7f
Abraham's descendant <tsholo.koketso@icloud.com>
2024-10-30 14:43:59 +0200

    mb

--- websockets/consumers/parent/parent_connect_async_functions.py
+++ websockets/consumers/parent/parent_connect_async_functions.py
@@ -22,9 +22,6 @@ def account_details(account, role):
 
         if not requesting_account.children.filter(school__none_compliant=False).exists():
             return {"denied": "Could not process your request, all of the children linked to your account have their accounts deactivated. For more information about this you can read our Termination Policy for why you're seeing this."}
-        
-        # Fetch announcements relevant to the user's school
-        unread_announcements_count = requesting_account.children.filter(school__announcements__accounts_reached=requesting_account).count()
 
         # Fetch unread messages for the user
         unread_messages_count = PrivateMessage.objects.filter(read_receipt=False).exclude(author=requesting_account).count()
@@ -34,7 +31,7 @@ def account_details(account, role):
         serialized_account = Serializer(instance=requesting_account).data
 
         # Return the serialized account details along with unread counts
-        return {'websocket_authenticated' : {'account': serialized_account, 'messages': unread_messages_count, 'announcements': unread_announcements_count}}
+        return {'websocket_authenticated' : {'account': serialized_account, 'messages': unread_messages_count}}
     
     except Exception as e:
         return {'error': str(e)}
