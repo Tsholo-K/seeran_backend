35a59be29259cf237809e7d0813ca804fd52e497
Abraham's descendant <tsholo.koketso@icloud.com>
2024-11-03 23:25:49 +0200

    update

--- seeran_backend/urls.py
+++ seeran_backend/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 
+from django.urls import path
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls') ),
     path('api/upld/', include('uploads.urls') ),
+    path('sentry-debug/', trigger_error),
 ]
