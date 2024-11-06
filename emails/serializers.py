23ebb258a03642da9ebdd6cfa0c0250be62596bb
Abraham's descendant <tsholo.koketso@icloud.com>
2024-11-06 14:06:09 +0200

    update

--- emails/serializers.py
+++ emails/serializers.py
@@ -7,8 +7,6 @@ from .models import Email
 
 class EmailMessagesSerializer(serializers.ModelSerializer):
 
-    assigned_to = serializers.SerializerMethodField()
-
     class Meta:
         model = Email
-        fields = ['body', 'is_incoming', 'received_at', 'created_at', 'updated_at']
+        fields = ['body', 'is_incoming', 'received_at']
