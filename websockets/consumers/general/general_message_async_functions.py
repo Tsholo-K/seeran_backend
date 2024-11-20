895db21a27c15c16afdbd281eff45aeb28d5c159
Abraham's descendant <tsholo.koketso@icloud.com>
2024-11-20 16:39:48 +0200

    message logic update

--- websockets/consumers/general/general_message_async_functions.py
+++ websockets/consumers/general/general_message_async_functions.py
@@ -8,8 +8,8 @@ from django.utils import timezone
 
 # models 
 from accounts.models import BaseAccount
-from private_chat_rooms.models import PrivateChatRoom
-from private_chat_room_messages.models import  PrivateMessage
+from private_chat_rooms.models import PrivateChatRoom, PrivateChatRoomMembership
+from private_chat_room_messages.models import PrivateMessage
 
 # serializers
 from accounts.serializers.general_serializers import BareAccountDetailsSerializer
@@ -27,21 +27,16 @@ def message_private(account, role, details):
     try:
         # Validate users
         if account == details.get('account'):
-            return {"error": "validation error. you can not send a text message to yourself, it violates database constraints and is therefore not allowed."}
+            return {"error": "Validation error: You cannot send a message to yourself. This violates database constraints and is not allowed."}
 
-        # Retrieve the requested user's account
-        requesting_user = BaseAccount.objects.get(account_id=account)
-
-        # Retrieve the requesting users account and related school in a single query using select_related
+        # Retrieve the requesting user's account and related attributes
         requesting_account = accounts_utilities.get_account_and_permission_check_attr(account, role)
 
-        # Retrieve the requested user's account
+        # Retrieve the requested user's account and related attributes
         requested_user = BaseAccount.objects.get(account_id=details.get('account'))
-
-        # Retrieve the requested users account and related school in a single query using select_related
         requested_account = accounts_utilities.get_account_and_permission_check_attr(details.get('account'), requested_user.role)
 
-        # Check permissions
+        # Perform permission checks
         permission_error = permission_checks.message(requesting_account, requested_account)
         if permission_error:
             return {'error': permission_error}
@@ -49,37 +44,47 @@ def message_private(account, role, details):
         with transaction.atomic():
             timestamp = timezone.now()
 
-            # Retrieve or create the chat room
-            private_chat_room, created = PrivateChatRoom.objects.get_or_create(participant_one=requesting_user if requesting_user.id < requested_user.id else requested_user, participant_two=requested_user if requesting_user.id < requested_user.id else requesting_user, defaults={'participant_one': requesting_user, 'participant_two': requested_user, 'latest_message_timestamp': timestamp})
-            
-            if created:
-                # Create the new message
-                new_private_chat_room_message = PrivateMessage.objects.create(author=requesting_user, message_content=details.get('message'), chat_room=private_chat_room, timestamp=timestamp)
-            
-            else:
-                # Retrieve the last message in the chat room
-                private_chat_room_last_message = private_chat_room.messages.order_by('-timestamp').first()
+            # Retrieve or create the chat room with the participants
+            chat_room = PrivateChatRoom.objects.filter(
+                participants=requesting_account
+            ).filter(participants=requested_user).first()
 
-                # Update the last message's 'last' field if it's from the same sender
-                if private_chat_room_last_message and private_chat_room_last_message.author == requesting_user:
-                    private_chat_room_last_message.last_message = False
-                    private_chat_room_last_message.save(update_fields=['last_message'])
+            if not chat_room:
+                # Create the new chat room and add participants
+                chat_room = PrivateChatRoom.objects.create(latest_message_timestamp=timestamp)
+                PrivateChatRoomMembership.objects.bulk_create([
+                    PrivateChatRoomMembership(chat_room=chat_room, participant=requesting_account),
+                    PrivateChatRoomMembership(chat_room=chat_room, participant=requested_user)
+                ])
 
-                # Create the new message
-                new_private_chat_room_message = PrivateMessage.objects.create(author=requesting_user, message_content=details.get('message'), chat_room=private_chat_room)
+            # Check if the latest message in the chat room is from the same sender and update it
+            last_message = chat_room.messages.order_by('-timestamp').first()
+            if last_message and last_message.author == requesting_account:
+                last_message.last_message = False
+                last_message.save(update_fields=['last_message'])
+
+            # Create a new message in the chat room
+            new_message = PrivateMessage.objects.create(
+                author=requesting_account,
+                message_content=details.get('message'),
+                chat_room=chat_room,
+                timestamp=timestamp
+            )
 
         # Serialize the new message
-        serialized_message = PrivateChatRoomMessageSerializer(new_private_chat_room_message, context={'participant': account}).data
+        serialized_message = PrivateChatRoomMessageSerializer(new_message, context={'participant': account}).data
 
-        serialized_author = BareAccountDetailsSerializer(requesting_user).data
+        # Serialize the author and recipient
+        serialized_author = BareAccountDetailsSerializer(requesting_account).data
         serialized_recipient = BareAccountDetailsSerializer(requested_user).data
 
         return {'message': serialized_message, 'author': serialized_author, 'recipient': serialized_recipient}
 
     except BaseAccount.DoesNotExist:
-        return {'error': 'Could not process your request, an account with the provided credentials does not exist. Please review the account details and try again.'}
+        return {'error': 'An account with the provided credentials does not exist. Please review the account details and try again.'}
 
     except Exception as e:
         return {'error': str(e)}
+
 
     
