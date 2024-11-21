7c77aa2d69aa2bde223f819de988c82d0e4749bc
Abraham's descendant <tsholo.koketso@icloud.com>
2024-11-21 16:45:26 +0200

    updates

--- authentication/decorators.py
+++ authentication/decorators.py
@@ -55,16 +55,7 @@ def token_required(view_func):
 
             # Calculate remaining lifespan of the token
             decoded_token = AccessToken(verified_access_token)
-            expiry_timestamp = decoded_token['exp']  # Expiration time in UNIX timestamp
-            current_timestamp = timezone.now().timestamp()
-            remaining_lifespan = int(expiry_timestamp - current_timestamp)  # In seconds
 
-            if remaining_lifespan <= 0:
-                return authentication_utilities.remove_authorization_cookies(JsonResponse(
-                    {'error': 'Could not process your request, your access credentials have expired.. request revoked'},
-                    status=status.HTTP_401_UNAUTHORIZED
-                ))
-            
             # Retrieve and set the authenticated user
             try:
                 requesting_account = BaseAccount.objects.get(pk=decoded_token['user_id'])
@@ -77,20 +68,7 @@ def token_required(view_func):
                 ))
 
             # Proceed to the view
-            response = view_func(request, *args, **kwargs)
-
-            # Set the token back with the calculated lifespan
-            response.set_cookie(
-                'access_token',
-                str(verified_access_token),
-                domain=settings.SESSION_COOKIE_DOMAIN,
-                samesite=settings.SESSION_COOKIE_SAMESITE,
-                secure=True,
-                httponly=True,
-                max_age=remaining_lifespan
-            )
-
-            return response
+            return view_func(request, *args, **kwargs)
 
         except Exception as e:
             # Log unexpected errors and provide a generic error response
